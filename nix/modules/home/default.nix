82e6b5a631d32d35e6ce5e322318a82de19afceb
Matteo Pierro <pierromatteo@gmail.com>
2026-09-21 16:56:29 +0200

    fix: expose Nix apps to Spotlight

--- nix/modules/home/default.nix
+++ nix/modules/home/default.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [ ./packages.nix ];
 
   programs.home-manager.enable = true;
   home.stateVersion = "24.11";
+
+  targets.darwin = {
+    linkApps.enable = false;
+    copyApps.enable = pkgs.stdenv.hostPlatform.isDarwin;
+  };
 }
