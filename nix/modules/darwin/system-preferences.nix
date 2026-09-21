ae52b0b2d55bbec40dfd177aab2ffbec4876482c
Matteo Pierro <pierromatteo@gmail.com>
2026-09-21 17:25:15 +0200

    feat: configure keyboard repeat timing

--- nix/modules/darwin/system-preferences.nix
+++ nix/modules/darwin/system-preferences.nix
@@ -1,5 +1,10 @@
 { ... }:
 {
+  system.defaults.NSGlobalDomain = {
+    KeyRepeat = 2;
+    InitialKeyRepeat = 15;
+  };
+
   system.defaults.dock = {
     autohide = true;
     orientation = "bottom";
